5114e4829b1f060e45d7a1bfb8b1506bd99302a4
Eden Sahilé <edensahile28@gmail.com>
2022-06-28 16:47:13 +0200

    router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import React from 'react'
+import {Routes, Route} from 'react-router-dom'
 import './App.css';
 import Navbar from './Components/Navbar';
 import Footer from './Components/Footer';
 import AddBooksContainer from './Containers/AddBooksContainer';
+import SearchBooks from './Containers/SearchBooks.js';
 
 function App() {
   return (
     <div className="App">
+      
      <Navbar/>
-     <AddBooksContainer/>
-     <Footer/>
+     <Routes>
+       <Route path='/' element={<AddBooksContainer/>}/>
+       <Route path='/search' element={<SearchBooks/>}/>
+   
+     </Routes>
+        <Footer/>
+        
     </div>
   );
 }
---
+++ src/Containers/SearchBooks.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SearchBooks = () => {
+  return (
+    <div>
+        Fetch Books from Google API
+    </div>
+  )
+}
+
+export default SearchBooks
